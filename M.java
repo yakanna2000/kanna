9879f7aea1eb6002d7058e951bde1828e592ac9c
yakanna <yakannakadem@gmail.com>
2025-04-09 16:05:05 +0530

    first commit

---
+++ M.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class M{
+    public static void main(String[] args){
+        System.out.println("kanna");
+    }
+}
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/yakanna2000/kanna.git
+// git push -u origin main
